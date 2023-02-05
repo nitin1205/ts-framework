8bacc022dc6bb1e354db32947559c4320feb997e
Nitin Singh <vortechcorp@gmail.com>
2023-02-05 21:52:11 +0530

    created User, Eventing and Sync classes and used composition to delegate work from users class to Eventing and sync class

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { User } from "./models/User";
+
+// const user = new User({name: 'myname', age: 18});
+
+// user.set({name: 'nitin'})
+
+// user.on('change', () => console.log('change no 1'));
+// user.on('change', () => console.log('change no 2'));
+// user.on('change', () => console.log('change no 3'));
+// user.on('save', () => console.log('save was trigger'));
+
+// console.log(user);
+// console.log(user.get('age'));
+// user.trigger('change');
+// user.trigger('save');
+
+const user = new User({ name: 'New Record', age: 0 });
+
+user.save();
