662fd729b524ee1c307d766f0dc77f75efcafefa
Nitin Singh <vortechcorp@gmail.com>
2023-02-06 20:26:28 +0530

    created Collection and UserForm class and in UserForm added method to render HTML and add enetlistener to HTML tags

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,7 @@
-import { User } from "./models/User";
+import { UserForm } from "./views/UserForm";
 
-// const user = new User({name: 'myname', age: 18});
+const userForm = new UserForm(
+    document.getElementById('root')
+);
 
-// user.set({name: 'nitin'})
-
-// user.on('change', () => console.log('change no 1'));
-// user.on('change', () => console.log('change no 2'));
-// user.on('change', () => console.log('change no 3'));
-// user.on('save', () => console.log('save was trigger'));
-
-// console.log(user);
-// console.log(user.get('age'));
-// user.trigger('change');
-// user.trigger('save');
-
-const user = User.buildUser({ id: 1 });
-
-user.on('change', () => {
-    console.log(user);
-});
-
-user.fetch();
+userForm.render();
